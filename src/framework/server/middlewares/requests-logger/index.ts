b84a89a3c12e12fd5dd2649465ef36249e64e1e2
Angel Ulise Tabares Gonzalez <taho2509@gmail.com>
2020-04-18 20:26:47 -0400

    add jwt authentication for update endpoint

--- src/framework/server/middlewares/requests-logger/index.ts
+++ src/framework/server/middlewares/requests-logger/index.ts
@@ -2,6 +2,9 @@ import logger from '../../../../interfaces/tools/logger'
 import { Middleware } from 'koa'
 
 const loggerMiddleware: Middleware = async (ctx, next): Promise<void> => {
+  try {
+    logger.info(`new request from ${ctx.state.user.aud} by ${ctx.state.user.sub}`)
+  } catch (err) {}
   await next()
   const rt = ctx.response.get('X-Response-Time')
   logger.info(`${ctx.method} ${ctx.url} - ${rt}`)
